1d7b7062f977e6181955fb5f7b3b73780d1f6f65
Remy Gosselin <gosselin.remy@gmail.com>
2023-09-11 15:43:25 +0400

    Soutenance blanche 1

--- profiles/views.py
+++ profiles/views.py
@@ -27,7 +27,6 @@ def profile(request, username):
     Parameters:
     - request: The HTTP request object.
     - username: The username of the profile to be displayed.
-    - coucou: dis coucou.
 
     Returns:
     - Rendered HTML response for the profile details page.
